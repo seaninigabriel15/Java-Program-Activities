04a88517af25b6104177a3bb6056f6807bd092a7
seaninigabriel15 <70614920+seaninigabriel15@users.noreply.github.com>
2021-02-11 13:56:09 +0800

    Create helloworld.java

---
+++ helloworld.java
@@ -0,0 +1,13 @@
+public class helloworld {
+
+    public static void main(String[] args) {
+        int sum;
+        String x = "this is life";
+        System.out.println(x);
+      
+        int e = 5;
+        int y = 9;
+        sum = e + y;
+        System.out.println(sum);
+    }
+}
